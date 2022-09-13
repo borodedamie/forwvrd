268bd6c5eb511977a04696ce7c80c5a2b3524c5b
ojara <ojarafavour@yahoo.com>
2022-09-13 17:39:38 +0100

    css changes on about page

--- src/components/aboutContent/AboutContent.jsx
+++ src/components/aboutContent/AboutContent.jsx
@@ -25,8 +25,9 @@ if ( state.succeeded ) {
 }
 
     return (
+    < div className='aboutPage'>
         <div className='aboutContent'>
-        <div className="row1">
+            <div className="row1">
             <h1 className='about-heading'>About</h1>
             <p className='about-sub-heading'>Tuesday 28 June 2022</p>
 
@@ -35,9 +36,9 @@ if ( state.succeeded ) {
                 <p>With its intuitive endless scroll design, content stays front and center, allowing you to immerse yourself in each piece and seamlessly move from one to the next. The intuitive “discover” arrow allows you to sort pieces by interest or search by keyword. Forward is super fast and responsive, and the combination of text and illustration look great on any screen (desktop, tablet, or phone).</p>
                 <p>Our team of writers, editors, illustrators, and designers, work thoughtfully to create features that are as insightful as they are easy to read - the very best content to keep you going forward.</p>
             </div>
-        </div>
+            </div>
 
-        <div className="row2">
+            <div className="row2">
             <div className="aboutImg">
                 <img src={aboutImg} alt="" />
             </div>
@@ -68,26 +69,26 @@ if ( state.succeeded ) {
                 <li className="role-name">Copy Editor</li>
                 <li className="role">Faladim Bomselak</li>
             </div>
-        </div>
-
-        <div className="row3">
-            <div className="about-title">
-                <h3>Contact</h3>
             </div>
 
-            <div className="roles-section">
+            <div className="row3">
+                <div className="about-title">
+                <h3>Contact</h3>
+                </div>
+
+                <div className="roles-section">
                 <li className="role-name">Submissions</li>
                 <li className="role">Submissions@getforward.com</li>
-            </div>
+                </div>
 
-            <div className="roles-section">
+                <div className="roles-section">
                 <li className="role-name">General/all enquiries</li>
                 <li className="role">info@getforward.com</li>
-            </div>
+                </div>
 
-            <div className="about-title">
+                <div className="about-title">
                 <h3>Newsletter</h3>
-            </div>
+                </div>
 
                 <li className="role">Subscribe to receive updates</li>
 
@@ -110,25 +111,30 @@ if ( state.succeeded ) {
                 </form>             
             </div>
 
+           
 
-            <div className="fixedFlex" style={{padding:'0'}}>
-            <div className="fixedLeft">
+           
+        </div>
+        <div className="fixedFlex" >
+                <div className="fixedLeft">
               <Link to="/about" reloadDocument="true"><h5 className="about">ABOUT</h5></Link>
               {/* <h5 onClick={ () => setAboutPage(true) } className='about'>ABOUT</h5> */}
 
                 <div className="terms">
                     <p>Terms and Conditions <br /> Privacy Policy</p>
               </div>
-            </div>
+                </div>
 
-            <div className="fixedRight">
+                <div className="fixedRight">
                 <div className="social-links">
                     <p>T</p>
                     <p>IG</p>
                 </div>
-            </div>
+                </div>
         </div>
-        </div>
+      
+
+    </div>  
     )
 }
 
